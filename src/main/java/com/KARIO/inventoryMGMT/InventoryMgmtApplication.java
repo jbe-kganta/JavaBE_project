11fd87d900b13cc4ea7051a2b10a7ba601d3bf7d
jbe-kganta <jbe.kganta@gmail.com>
2024-11-07 04:56:00 -0700

    added initial commit

---
+++ src/main/java/com/KARIO/inventoryMGMT/InventoryMgmtApplication.java
@@ -0,0 +1,14 @@
+package com.KARIO.inventoryMGMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMgmtApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CARS inventory Management");
+		System.out.println("This helps to build inventory management project");
+	}
+
+}
---
+++ src/test/java/com/KARIO/inventoryMGMT/InventoryMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KARIO.inventoryMGMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
